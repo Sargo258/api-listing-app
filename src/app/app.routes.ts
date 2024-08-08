dc30cf00b03e82b4d9c3e30221adbeeacf39d2a0
santiago <santiagorg218@gmail.com>
2024-08-08 16:00:35 -0500

    se grego tailwind css y se cambiaron os estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/character', pathMatch: 'full' },
   {
     path: 'character',
     loadComponent: () => import('./components/character-list/character-list.component').then(m => m.CharacterListComponent)
